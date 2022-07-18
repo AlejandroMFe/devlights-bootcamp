88e89d705a88bfaeb8e5018e13087865e0fb30f6
Nemo3003 <ulimarian2000@gmail.com>
2022-07-18 20:57:16 -0300

    Landing page: trabajando en ello

--- GYM-UI/src/app/app.module.ts
+++ GYM-UI/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import{ Navbar } from './components/navbar/navbar.component';
+import{ Footer } from './components/footer/footer.component';
+import{ Section } from './components/section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Navbar,
+    Footer,
+    Section
   ],
   imports: [
     BrowserModule,
---
+++ GYM-UI/src/app/components/section/section.component.ts
@@ -0,0 +1,10 @@
+import {Component}from '@angular/core';
+
+@Component({
+    selector: 'Section',
+    templateUrl: './section.component.html',
+    styleUrls: ['./section.component.css'],
+})
+export class Section{
+    title="Bienvenidos a GYM-APP"
+}
