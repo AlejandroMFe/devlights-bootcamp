257540f1a51ebf85f362a05b128c6a320d80a1a2
Alejandro <alejandro.mfe@gmail.com>
2022-07-22 20:48:07 -0300

    add dbContext and generate init migration

---
+++ Back/DataAccess/DbGymContext.cs
@@ -0,0 +1,21 @@
+﻿using GymDataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymDataAccess;
+public class DbGymContext : DbContext
+{
+    public DbGymContext(DbContextOptions<DbGymContext> options) : base(options)
+    {        
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // No mapea esta entidad DaysAndHours
+        // Sirve para hacer consultas
+        modelBuilder.Entity<Activity>().Ignore(a => a.DaysAndHours); 
+        
+        // La entidad DayAndHour no tiene Id
+        modelBuilder.Entity<DayAndHour>().HasNoKey();
+    }
+    public DbSet<Activity> Activities { get; set; }
+    public DbSet<Student> Students { get; set; }
+}
---
+++ Back/DataAccess/Migrations/20220722233714_init.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymDataAccess.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Activities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Professor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Capacity = table.Column<int>(type: "int", nullable: false),
+                    Hour = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DayAndHour",
+                columns: table => new
+                {
+                    Day = table.Column<int>(type: "int", nullable: false),
+                    HourStart = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HourEnd = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ActivityId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ActivityStudent",
+                columns: table => new
+                {
+                    ActivitiesId = table.Column<int>(type: "int", nullable: false),
+                    StudentsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityStudent", x => new { x.ActivitiesId, x.StudentsId });
+                    table.ForeignKey(
+                        name: "FK_ActivityStudent_Activities_ActivitiesId",
+                        column: x => x.ActivitiesId,
+                        principalTable: "Activities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ActivityStudent_Students_StudentsId",
+                        column: x => x.StudentsId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActivityStudent_StudentsId",
+                table: "ActivityStudent",
+                column: "StudentsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActivityStudent");
+
+            migrationBuilder.DropTable(
+                name: "DayAndHour");
+
+            migrationBuilder.DropTable(
+                name: "Activities");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
