61e11a7771d5f9dcbc9b1df38c8d8665104ca246
Alejandro <alejandro.mfe@gmail.com>
2022-07-28 21:34:04 -0300

    add many-to-many migration

--- Back/DataAccess/Migrations/20220729003256_many-to-many-ActivityStudent.Designer.cs
+++ Back/DataAccess/Migrations/20220729003256_many-to-many-ActivityStudent.Designer.cs
@@ -4,6 +4,7 @@ using GymDataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GymDataAccess.Migrations
 {
     [DbContext(typeof(GymDbContext))]
-    partial class DbGymContextModelSnapshot : ModelSnapshot
+    [Migration("20220729003256_many-to-many-ActivityStudent")]
+    partial class manytomanyActivityStudent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
