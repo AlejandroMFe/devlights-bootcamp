e613b7b9449868799c1e111dbdf3f2fc2bae784a
Alejandro <alejandro.mfe@gmail.com>
2022-08-05 12:11:25 -0300

    refactoring price component

--- GYM-UI/src/app/components/Pricing/Pricing.component.ts
+++ GYM-UI/src/app/components/Pricing/Pricing.component.ts
@@ -1,17 +1,17 @@
-import {Component}from '@angular/core';
+import { Component } from '@angular/core';
 
 
 @Component({
     selector: 'Pricing',
     templateUrl: './Pricing.component.html',
-    styleUrls: ['./Pricing.component.css'],
+    styleUrls: [ './Pricing.component.css' ],
 })
-export class Pricing{
+export class Pricing {
     title = 'Pricing';
-    
-    classes = ['Basic', 'Intermediate', 'Advanced'] ;
-    prices = [120,150,250];
-    features = ['1 month unlimited classes', '6 months unlimited classes', '12 months unlimited classes'];
-    description = ['The most affordable price and a wonderful experience', 'The most flexible price and a wonderful experience', 'The most expensive price but a more customizable experience'];
-    images = ['assets/images/basic.jpg', 'assets/images/intermediate.jpg', 'assets/images/advanced.jpg'];
+
+    classes = [ 'Basic', 'Intermediate', 'Advanced' ];
+    prices = [ 1200, 1750, 3500 ];
+    features = [ '1 month unlimited classes', '6 months unlimited classes', '12 months unlimited classes' ];
+    description = [ 'The most affordable price and a wonderful experience', 'The most flexible price and a wonderful experience', 'The most expensive price but a more customizable experience' ];
+    images = [ 'assets/images/basic.jpg', 'assets/images/intermediate.jpg', 'assets/images/advanced.jpg' ];
 }
