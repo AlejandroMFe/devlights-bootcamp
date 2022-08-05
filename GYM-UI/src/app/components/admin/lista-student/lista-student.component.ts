60b5889f125654e2e736f2cac91d53d7c64047be
Alejandro <alejandro.mfe@gmail.com>
2022-08-05 09:38:41 -0300

    refactoring-lista-student

--- GYM-UI/src/app/components/admin/lista-student/lista-student.component.ts
+++ GYM-UI/src/app/components/admin/lista-student/lista-student.component.ts
@@ -1,112 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Activity } from 'src/app/_models/activity';
 
 @Component({
   selector: 'app-lista-student',
   templateUrl: './lista-student.component.html',
-  styleUrls: ['./lista-student.component.css']
+  styleUrls: [ './lista-student.component.css' ]
 })
-export class ListaStudentComponent{
-  title = 'Lista de estudiantes';
-  students = [
-    {
-      name: 'Juan',
-      lastname: 'Perez',
-      age: '20',
-      email: 'applied@example.com',
-      class: 'zumba'
-    },
-    {
-      name: 'Pedro',
-      lastname: 'Perez',
-      age: '20',
-      email: 'applied@example.com',
-      class: 'zumba'
-    },
-    {
-      name: 'Juana',
-      lastname: 'Perez',
-      age: '20',
-      email: 'applied@example.com',
-      class: 'zumba'
-    }
-    ,
-    {
-      name: 'Julian',
-      lastname: 'Perez',
-      age: '20',
-      email: 'applied@example.com',
-      class: 'zumba'
-    }
-    ,
-    {
-      name: 'Juan Pablo',
-      lastname: 'Perez',
-      age: '20',
-      email: 'applied@example.com',
-      class: 'ABS'
-    }
-    ,
-    {
-      name: 'Sebastian',
-      lastname: 'Perez',
-      age: '20',
-      email: 'applied@example.com',
-      class: 'ABS'
-    }
-    ,
-    {
-      name: 'JMarilyn',
-      lastname: 'Perez',
-      age: '20',
-      email: 'applied@example.com',
-      class: 'WeightLift'
-    }
-    ,
-    {
-      name: 'Ryan',
-      lastname: 'Perez',
-      age: '20',
-      email: 'applied@example.com',
-      class: 'WeightLift'
-    }
-    ,
-    {
-      name: 'Steffanie',
-      lastname: 'Perez',
-      age: '20',
-      email: 'applied@example.com',
-      class: 'Pilates'
-    }
-    ,
-    {
-      name: 'Juana',
-      lastname: 'Sanchez',
-      age: '20',
-      email: 'applied@example.com',
-      class: 'Pilates'
-    }
-    ,
-    {
-      name: 'Leandro',
-      lastname: 'Perez',
-      age: '20',
-      email: 'applied@example.com',
-      class: 'Pilates'
-    }
-    ,
-    {
-      name: 'Julian',
-      lastname: 'Perezas',
-      age: '20',
-      email: 'applied@example.com',
-      class: 'Pilates'
-    }
-    ,
-    {
-      name: 'Ana',
-      lastname: 'Perez',
-      age: '20',
-      email: 'applied@example.com',
-      class: 'Pilates'
-    }];
+export class ListaStudentComponent implements OnInit {
+  studentsActivities: any[];
+  count: number = 1;
+
+  constructor() {
+    this.studentsActivities = Activity.getStudentsActivities();
+  }
+  
+  increaseCount() {
+    return this.count++;
+  }
+
+  ngOnInit(): void {
+  }
 }
